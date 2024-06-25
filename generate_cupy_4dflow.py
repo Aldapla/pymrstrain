ae5094a661af300ebd989b9ba63f39cb98a0c13b
Hernan Mella <hernan.mella@pucv.cl>
2024-06-24 21:59:29 -0400

    Fixed unnecessary conversions from numpy to cupy

--- generate_cupy_4dflow.py
+++ generate_cupy_4dflow.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import time
+from pathlib import Path
 
 import cupy as cp
 import matplotlib.pyplot as plt
@@ -17,6 +18,7 @@ from PyMRStrain.MPIUtilities import MPI_comm, MPI_rank, gather_image
 from PyMRStrain.MRImaging import SliceProfile
 from PyMRStrain.Phantom import femPhantom
 
+
 def FlowImage3D(M, kxyz, t ,v, r0, gamma_x_delta_B0, T2, VENC, profile):
   # Number of kspace lines/spokes/interleaves
   nb_lines = kxyz[0].shape[1] # kxy[0].cols()
@@ -32,21 +34,21 @@ def FlowImage3D(M, kxyz, t ,v, r0, gamma_x_delta_B0, T2, VENC, profile):
 
   # Get the equivalent gradient needed to go from the center of the kspace
   # to each location
-  kx = 2.0 * np.pi * kxyz[0]
-  ky = 2.0 * np.pi * kxyz[1]
-  kz = 2.0 * np.pi * kxyz[2]
+  kx = 2.0 * cp.pi * kxyz[0]
+  ky = 2.0 * cp.pi * kxyz[1]
+  kz = 2.0 * cp.pi * kxyz[2]
 
   # Copy blood position to estimate the current position using the approximation r(t0+dt) = r0 + v0*dt
-  r = cp.asarray( np.zeros([nb_spins, 3], dtype=np.float32) , dtype=cp.float32)
+  r = cp.zeros([nb_spins, 3], dtype=cp.float32)
 
   # Kspace and Fourier exponential
   Mxy = 1.0e+3 * nb_spins * cp.exp(1j * np.pi / VENC * v) * profile
-  fe_xy = cp.asarray( np.zeros([nb_spins, 1], dtype=np.float32), dtype=cp.float32)
-  fe = cp.asarray( np.zeros([nb_spins, 1], dtype=np.complex64), dtype=cp.complex64)
-  phi_off = cp.asarray( np.zeros([nb_spins, 1], dtype=np.float32), dtype=cp.float32)
+  fe_xy = cp.zeros([nb_spins, 1], dtype=cp.float32)
+  fe    = cp.zeros([nb_spins, 1], dtype=cp.complex64)
+  phi_off = cp.zeros([nb_spins, 1],  dtype=cp.float32)
 
   # kspace
-  kspace = cp.asarray( np.zeros([nb_meas, nb_lines, nb_kz, 3], dtype=np.complex64) , dtype=cp.complex64)
+  kspace = cp.zeros([nb_meas, nb_lines, nb_kz, 3], dtype=cp.complex64)
 
   # T2* decay
   T2_decay = cp.exp(-t / T2)
@@ -93,7 +95,7 @@ if __name__ == '__main__':
   FOV = np.array(pars['Imaging']['FOV'])
   RES = np.array(pars['Imaging']['RES'])
   T2star = pars['Imaging']['T2star']/1000.0
-  VENC = np.array(pars['Imaging']['VENC'])
+  VENC = pars['Imaging']['VENC']
   OFAC = pars['Imaging']['OVERSAMPLING']
 
   # Hardware parameters
@@ -105,8 +107,8 @@ if __name__ == '__main__':
   theta_x = np.deg2rad(pars['Formatting']['theta_x'])
   theta_y = np.deg2rad(pars['Formatting']['theta_y'])
   theta_z = np.deg2rad(pars['Formatting']['theta_z'])
-  MPS_ori = Rz(theta_z)@Rx(theta_x)@Ry(theta_y)
-  LOC = np.array(pars['Formatting']['LOC'])
+  MPS_ori = cp.asarray(Rz(theta_z)@Rx(theta_x)@Ry(theta_y), dtype=cp.float32)
+  LOC = cp.asarray((pars['Formatting']['LOC']), dtype=cp.float32)
 
   # Imaging sequences for the image generation
   sequences = ['FFE', 'EPI']
@@ -119,6 +121,7 @@ if __name__ == '__main__':
 
     # Create FEM phantom object
     phantom = femPhantom(path=path_NS, vel_label='velocity', scale_factor=0.01)
+    cp_nodes = cp.asarray(phantom.mesh['nodes'], dtype=cp.float32)
 
     # Assemble mass matrix for integrals (just once)
     M = cp_csr_matrix(massAssemble(phantom.mesh['elems'], phantom.mesh['nodes']))
@@ -133,11 +136,10 @@ if __name__ == '__main__':
     gamma_x_delta_B0 = cp.asarray(2*np.pi*gammabar*delta_B0, dtype=cp.float32)
 
     # Path to export the generated data
-    im_folder = 'MRImages/'
+    export_path = Path('MRImages/{:s}_V{:.0f}.pkl'.format(seq, 100.0*VENC))
 
     # Make sure the directory exist
-    if MPI_rank==0:
-      os.makedirs(im_folder, exist_ok=True)
+    os.makedirs(str(export_path.parent), exist_ok=True)
 
     # Generate kspace trajectory
     lps = pars[seq]['LinesPerShot']
@@ -150,24 +152,24 @@ if __name__ == '__main__':
     traj.times  = cp.asarray(traj.times, dtype=cp.float32)
 
     # Translate phantom to obtain the desired slice location
-    nodes = cp.asarray((phantom.mesh['nodes'] - traj.LOC)@traj.MPS_ori, dtype=cp.float32)
+    cp_nodes = (cp_nodes - traj.LOC)@traj.MPS_ori
 
     # Slice profile
     # gammabar = 1.0e+6*42.58 # Hz/T 
     # G_z = 1.0e-3*30.0       # [T/m]
     delta_z = FOV[2]        # [m]
     # delta_g = gammabar*G_z*delta_z
-    profile = cp.asarray(np.tile(SliceProfile(z=nodes[:,2], delta_z=delta_z).profile[:, np.newaxis], (1, 3)), dtype=cp.float32)
+    profile = cp.asarray(np.tile(SliceProfile(z=cp_nodes[:,2], delta_z=delta_z).profile[:, np.newaxis], (1, 3)), dtype=cp.float32)
     # profile = np.ones_like(nodes[:,2])
 
     # Print echo time
-    if MPI_rank==0: print('Echo time = {:.1f} ms'.format(1000.0*traj.echo_time))
+    print('Echo time = {:.1f} ms'.format(1000.0*traj.echo_time))
 
     # kspace array
     ro_samples = traj.ro_samples
     ph_samples = traj.ph_samples
     slices = traj.slices
-    K = cp.asarray(np.zeros([ro_samples, ph_samples, slices, 3, phantom.Nfr], dtype=np.complex64), dtype=cp.complex64)
+    K = cp.zeros([ro_samples, ph_samples, slices, 3, phantom.Nfr], dtype=cp.complex64)
 
     # List to store how much is taking to generate one volume
     times = []
@@ -177,35 +179,30 @@ if __name__ == '__main__':
 
       # Read velocity data in frame fr
       phantom.read_data(fr)
-      velocity = cp.asarray(phantom.velocity@traj.MPS_ori, dtype=cp.float32)
+      cp_velocity = cp.asarray(phantom.velocity, dtype=cp.float32)
+      velocity = cp_velocity@traj.MPS_ori
 
       # Generate 4D flow image
-      if MPI_rank == 0: print('Generating frame {:d}'.format(fr))
+      print('Generating frame {:d}'.format(fr))
       t0 = time.time()
-      K[traj.local_idx,:,:,:,fr] = FlowImage3D(M, traj.points, traj.times, velocity, nodes, gamma_x_delta_B0, T2star, VENC, profile)
+      K[traj.local_idx,:,:,:,fr] = FlowImage3D(M, traj.points, traj.times, cp_velocity, cp_nodes, gamma_x_delta_B0, T2star, VENC, profile)
       t1 = time.time()
       times.append(t1-t0)
 
       # Save kspace for debugging purposes
       if preview:
-        K_copy = np.copy(K)
-        K_copy = gather_image(K_copy)
-        if MPI_rank==0:
-          np.save(export_path, K_copy)
+        with open(str(export_path), 'wb') as f:
+          pickle.dump({'kspace': cp.asnumpy(K), 'MPS_ori': cp.asnumpy(MPS_ori), 'LOC': cp.asnumpy(LOC)}, f)
 
       # Synchronize MPI processes
       print(np.array(times).mean())
-      MPI_comm.Barrier()
 
     # Show mean time that takes to generate each 3D volume
     print(np.array(times).mean())
 
-    # Gather results
-    K = gather_image(K)
-
     # Export generated data
-    if MPI_rank==0:
-      # K_scaled = scale_data(K, mag=False, real=True, imag=True, dtype=np.uint64)
-      np.save(export_path, K)
+    # K_scaled = scale_data(K, mag=False, real=True, imag=True, dtype=np.uint64)
+    with open(str(export_path), 'wb') as f:
+      pickle.dump({'kspace': cp.asnumpy(K), 'MPS_ori': cp.asnumpy(MPS_ori), 'LOC': cp.asnumpy(LOC)}, f)
 
   stream.synchronize()
