90008f9c73f021e426b34a231c8590d27d40a953
Hernan Mella <hernan.mella@pucv.cl>
2024-06-25 17:03:48 -0400

    Removed sigpy nufft for EPI recons

--- plot_mr_image.py
+++ plot_mr_image.py
@@ -3,72 +3,29 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import yaml
-from sigpy.fourier import ifft, nufft_adjoint
 
 from PyMRStrain.Filters import Tukey_filter
-from PyMRStrain.Math import Rx, Ry, Rz, itok, ktoi
+from PyMRStrain.KSpaceTraj import Cartesian, Radial, Spiral
+from PyMRStrain.Math import itok, ktoi
 from PyMRStrain.Noise import add_cpx_noise
 from PyMRStrain.Plotter import multi_slice_viewer
-from PyMRStrain.KSpaceTraj import Cartesian, Radial, Spiral
-
-def nufft_recon(traj, K):
-  # Non-uniform fft
-  x0 = traj.points[0].flatten().reshape((-1,1))
-  x1 = traj.points[1].flatten().reshape((-1,1))
-  x2 = traj.points[2].flatten().reshape((-1,1))
-  x0 *= RES[0]//2/x0.max()
-  x1 *= RES[1]//2/x1.max()
-  x2 *= RES[2]//2/x2.max()
-  if isinstance(traj, Cartesian):
-    dcf = 1.0
-  else:
-    dcf = (x0**2 + x1**2 + x2**2)**0.5
-  kxkykz = np.concatenate((x0, x1, x2), axis=1)   # flattened kspace coordinates
-  y = K.flatten().reshape((-1,1))          # flattened kspace measures
-  image = nufft_adjoint(y*dcf, kxkykz, RES)  # inverse nufft
-  return image
 
 if __name__ == '__main__':
 
-  # Import imaging parameters
-  with open('PARAMETERS.yaml') as file:
-    pars = yaml.load(file, Loader=yaml.FullLoader)
-
-  # Imaging parameters
-  FOV = np.array(pars["Imaging"]["FOV"])
-  RES = np.array(pars["Imaging"]["RES"])
-  T2star = pars["Imaging"]["T2star"]/1000.0
-  VENC = pars["Imaging"]["VENC"]*100.0
-  OFAC = pars["Imaging"]["OVERSAMPLING"]
-  dt = pars["Imaging"]["TIMESPACING"]
-
-  # Hardware parameters
-  G_sr  = pars["Hardware"]["G_sr"]
-  G_max = pars["Hardware"]["G_max"]
-  r_BW  = pars["Hardware"]["r_BW"]
-
-  # Imaging orientation parameters
-  theta_x = np.deg2rad(pars["Formatting"]["theta_x"])
-  theta_y = np.deg2rad(pars["Formatting"]["theta_y"])
-  theta_z = np.deg2rad(pars["Formatting"]["theta_z"])
-  LOCs = np.array(pars["Formatting"]["LOC"])
-
-  # Build rotation matrix and get slice location
-  MPS_ori = Rz(theta_z)@Rx(theta_x)@Ry(theta_y)
-  LOC = LOCs
-
-  # Sequence
-  seq  = 'EPI'
-  lps = pars[seq]['LinesPerShot']
-  traj = Cartesian(FOV=FOV, res=RES, oversampling=OFAC, lines_per_shot=lps, VENC=VENC, MPS_ori=MPS_ori, LOC=LOC, receiver_bw=r_BW, Gr_max=G_max, Gr_sr=G_sr, plot_seq=False)
-
-  # Import generated data
-  im = 'MRImages/{:s}_V{:.0f}.pkl'.format(seq, VENC)
-  with open(im, 'rb') as f:
+  # Import image kspace
+  im_file = 'MRImages/FFE_V250.pkl'
+  with open(im_file, 'rb') as f:
     data = pickle.load(f)
 
   # Extract information from data
   K = data['kspace']
+  MPS_ori = data['MPS_ori']
+  LOC = data['LOC']
+  traj = data['traj']
+  RES = traj.res
+  print(traj.points[0].min(), traj.points[0].max(), print(1.0/(traj.points[0].max()-traj.points[0].min())))
+  print(traj.points[1].min(), traj.points[1].max(), print(1.0/(traj.points[1].max()-traj.points[1].min())))
+  print(traj.points[2].min(), traj.points[2].max(), print(1.0/(traj.points[2].max()-traj.points[2].min())))
 
   # # Zero padding in the dimensions with even measurements to avoid shifts in 
   # # the image domain
@@ -96,18 +53,25 @@ if __name__ == '__main__':
   K_fil = H*K
 
   # Fix the direction of kspace lines measured in the opposite direction
-  if seq in 'EPI':
-    I = np.zeros(K_fil[::2,...].shape, dtype=np.complex64)
-    for v in range(I.shape[3]):
-      for fr in [0,1,2]:#range(I.shape[4]):
-        I[...,v,fr] = nufft_recon(traj, K_fil[...,v,fr])
-  else:
-    I = ktoi(K_fil[::2,...],[0,1,2])
+  if isinstance(traj, Cartesian) and traj.lines_per_shot > 1:   
+    # Reorder lines depending of their readout direction
+    for ph in range(K_fil.shape[1]):
+      # The first line in every shot should be ordered from left to right
+      if ph % traj.lines_per_shot == 0:
+        ro = 1
+
+      # Reverse orientations (only when ro=-1)
+      K_fil[::ro,ph,...] = K_fil[::1,ph,...]
+
+      # Reverse readout
+      ro = -ro
+
+  # Reconstruct image
+  I = ktoi(K_fil[::2,...],[0,1,2])
 
   # Show figure
-  for fr in range(K.shape[-1]):
-    for i in [2]:
-      M = np.abs(I[:,:,:,i,fr])
-      P = np.angle(I[:,:,:,i,fr])
-      multi_slice_viewer([M, P])
-      plt.show()
+  for i in range(I.shape[3]):
+    M = np.abs(I[...,i,:])
+    P = np.angle(I[...,i,:])
+    multi_slice_viewer([M, P])
+    plt.show()
