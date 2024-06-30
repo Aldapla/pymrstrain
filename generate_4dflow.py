d4dd10f1dd38c5b7d575ace6076cf93be3a81ebf
hmella <hernan.mella@pucv.cl>
2024-06-30 16:55:11 -0400

    Removed redundant files and added matplotlib plotting using PyMRStrain.Plotter.MRIPlotter class to visualize_image.py

--- generate_4dflow.py
+++ generate_4dflow.py
@@ -3,8 +3,6 @@ import pickle
 import time
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import meshio
 import numpy as np
 import yaml
 
@@ -15,7 +13,7 @@ from PyMRStrain.KSpaceTraj import Cartesian
 from PyMRStrain.Math import Rx, Ry, Rz, itok, ktoi
 from PyMRStrain.MPIUtilities import MPI_comm, MPI_rank, gather_image
 from PyMRStrain.MRImaging import SliceProfile
-from PyMRStrain.Phantom import femPhantom
+from PyMRStrain.Phantom import FEMPhantom
 
 if __name__ == '__main__':
 
@@ -46,95 +44,92 @@ if __name__ == '__main__':
   LOC = np.array(pars['Formatting']['LOC'])
 
   # Imaging sequences for the image generation
-  sequences = ['FFE'] #['FFE', 'EPI']
+  sequence = 'FFE' # 'FFE' or 'EPI' (SPIRAL and RADIAL are also options)
 
-  # Iterate over sequences
-  for seq in sequences:
+  # Navier-Stokes simulation data to be used
+  phantom_file = 'phantom/phantom.xdmf'
 
-    # Navier-Stokes simulation data to be used
-    path_NS = 'phantom/phantom.xdmf'
+  # Create FEM phantom object
+  phantom = FEMPhantom(path=phantom_file, vel_label='velocity', scale_factor=0.01)
 
-    # Create FEM phantom object
-    phantom = femPhantom(path=path_NS, vel_label='velocity', scale_factor=0.01)
+  # Assemble mass matrix for integrals (just once)
+  M = massAssemble(phantom.mesh['elems'], phantom.mesh['nodes'])
 
-    # Assemble mass matrix for integrals (just once)
-    M = massAssemble(phantom.mesh['elems'], phantom.mesh['nodes'])
+  # Field inhomogeneity
+  x =  phantom.mesh['nodes']
+  gammabar = 1.0e+6*42.58 # Hz/T 
+  delta_B0 = x[:,0] + x[:,1] + x[:,2]  # spatial distribution
+  delta_B0 /= np.abs(delta_B0).max()  # normalization
+  delta_B0 *= 1.5*1e-6  # scaling (1 ppm of 1.5T)        
+  delta_B0 *= 0.0  # additional scaling (just for testing)
+  gamma_x_delta_B0 = 2*np.pi*gammabar*delta_B0
 
-    # Field inhomogeneity
-    x =  phantom.mesh['nodes']
-    gammabar = 1.0e+6*42.58 # Hz/T 
-    delta_B0 = x[:,0] + x[:,1] + x[:,2]  # spatial distribution
-    delta_B0 /= np.abs(delta_B0).max()  # normalization
-    delta_B0 *= 1.5*1e-6  # scaling (1 ppm of 1.5T)        
-    delta_B0 *= 0.0  # additional scaling (just for testing)
-    gamma_x_delta_B0 = 2*np.pi*gammabar*delta_B0
+  # Path to export the generated data
+  export_path = Path('MRImages/{:s}_V{:.0f}.pkl'.format(sequence, 100.0*VENC))
 
-    # Path to export the generated data
-    export_path = Path('MRImages/{:s}_V{:.0f}.pkl'.format(seq, 100.0*VENC))
+  # Make sure the directory exist
+  os.makedirs(str(export_path.parent), exist_ok=True)
 
-    # Make sure the directory exist
-    os.makedirs(str(export_path.parent), exist_ok=True)
+  # Generate kspace trajectory
+  lps = pars[sequence]['LinesPerShot']
+  traj = Cartesian(FOV=FOV, res=RES, oversampling=OFAC, lines_per_shot=lps, VENC=VENC, MPS_ori=MPS_ori, LOC=LOC, receiver_bw=r_BW, Gr_max=G_max, Gr_sr=G_sr, plot_seq=False)
 
-    # Generate kspace trajectory
-    lps = pars[seq]['LinesPerShot']
-    traj = Cartesian(FOV=FOV, res=RES, oversampling=OFAC, lines_per_shot=lps, VENC=VENC, MPS_ori=MPS_ori, LOC=LOC, receiver_bw=r_BW, Gr_max=G_max, Gr_sr=G_sr, plot_seq=False)
+  # Translate phantom to obtain the desired slice location
+  nodes = (phantom.mesh['nodes'] - traj.LOC)@traj.MPS_ori
 
-    # Translate phantom to obtain the desired slice location
-    nodes = (phantom.mesh['nodes'] - traj.LOC)@traj.MPS_ori
+  # Slice profile
+  # gammabar = 1.0e+6*42.58 # Hz/T 
+  # G_z = 1.0e-3*30.0       # [T/m]
+  delta_z = FOV[2]        # [m]
+  # delta_g = gammabar*G_z*delta_z
+  profile = SliceProfile(z=nodes[:,2], delta_z=delta_z).profile
+  # profile = np.ones_like(nodes[:,2])
 
-    # Slice profile
-    # gammabar = 1.0e+6*42.58 # Hz/T 
-    # G_z = 1.0e-3*30.0       # [T/m]
-    delta_z = FOV[2]        # [m]
-    # delta_g = gammabar*G_z*delta_z
-    profile = SliceProfile(z=nodes[:,2], delta_z=delta_z).profile
-    # profile = np.ones_like(nodes[:,2])
+  # Print echo time
+  if MPI_rank==0: print('Echo time = {:.1f} ms'.format(1000.0*traj.echo_time))
 
-    # Print echo time
-    if MPI_rank==0: print('Echo time = {:.1f} ms'.format(1000.0*traj.echo_time))
+  # kspace array
+  ro_samples = traj.ro_samples
+  ph_samples = traj.ph_samples
+  slices = traj.slices
+  K = np.zeros([ro_samples, ph_samples, slices, 3, phantom.Nfr], dtype=np.complex64)
 
-    # kspace array
-    ro_samples = traj.ro_samples
-    ph_samples = traj.ph_samples
-    slices = traj.slices
-    K = np.zeros([ro_samples, ph_samples, slices, 3, phantom.Nfr], dtype=np.complex64)
+  # List to store how much is taking to generate one volume
+  times = []
 
-    # List to store how much is taking to generate one volume
-    times = []
+  # Iterate over cardiac phases
+  for fr in range(phantom.Nfr):
 
-    # Iterate over cardiac phases
-    for fr in [0]:#range(phantom.Nfr):
+    # Read velocity data in frame fr
+    phantom.read_data(fr)
+    velocity = phantom.velocity@traj.MPS_ori
 
-      # Read velocity data in frame fr
-      phantom.read_data(fr)
-      velocity = phantom.velocity@traj.MPS_ori
+    # Generate 4D flow image
+    if MPI_rank == 0: print('Generating frame {:d}'.format(fr))
+    t0 = time.time()
+    K[traj.local_idx,:,:,:,fr] = FlowImage3D(MPI_rank, M, traj.local_points, traj.local_times, velocity, nodes, gamma_x_delta_B0, T2star, VENC, profile)
+    t1 = time.time()
+    times.append(t1-t0)
 
-      # Generate 4D flow image
-      if MPI_rank == 0: print('Generating frame {:d}'.format(fr))
-      t0 = time.time()
-      K[traj.local_idx,:,:,:,fr] = FlowImage3D(MPI_rank, M, traj.local_points, traj.local_times, velocity, nodes, gamma_x_delta_B0, T2star, VENC, profile)
-      t1 = time.time()
-      times.append(t1-t0)
+    # Save kspace for debugging purposes
+    if preview:
+      K_copy = np.copy(K)
+      K_copy = gather_image(K_copy)
+      if MPI_rank==0:
+        with open(str(export_path), 'wb') as f:
+          pickle.dump({'kspace': K_copy, 'MPS_ori': MPS_ori, 'LOC': LOC, 'traj': traj}, f)
 
-      # Save kspace for debugging purposes
-      if preview:
-        K_copy = np.copy(K)
-        K_copy = gather_image(K_copy)
-        if MPI_rank==0:
-          with open(str(export_path), 'wb') as f:
-            pickle.dump({'kspace': K_copy, 'MPS_ori': MPS_ori, 'LOC': LOC, 'traj': traj}, f)
-
-      # Synchronize MPI processes
-      print(np.array(times).mean())
-      MPI_comm.Barrier()
-
-    # Show mean time that takes to generate each 3D volume
+    # Synchronize MPI processes
     print(np.array(times).mean())
+    MPI_comm.Barrier()
 
-    # Gather results
-    K = gather_image(K)
+  # Show mean time that takes to generate each 3D volume
+  print(np.array(times).mean())
 
-    # Export generated data
-    if MPI_rank==0:
-      with open(str(export_path), 'wb') as f:
-        pickle.dump({'kspace': K, 'MPS_ori': MPS_ori, 'LOC': LOC, 'traj': traj}, f)
+  # Gather results
+  K = gather_image(K)
+
+  # Export generated data
+  if MPI_rank==0:
+    with open(str(export_path), 'wb') as f:
+      pickle.dump({'kspace': K, 'MPS_ori': MPS_ori, 'LOC': LOC, 'traj': traj}, f)
