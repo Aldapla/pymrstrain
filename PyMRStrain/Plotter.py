1990795c4210fb23977c22691902eeb5e333eef6
hmella <hernan.mella@pucv.cl>
2024-08-03 10:31:39 -0400

    Added colorbar to each axis. Also added subplots shape and swap_axes to the class constructor

--- PyMRStrain/Plotter.py
+++ PyMRStrain/Plotter.py
@@ -3,26 +3,33 @@ import numpy as np
 
 
 class MRIPlotter:
-  def __init__(self, images=[], caxis=None, cmap=plt.get_cmap('Greys_r'), title='Slice', next_frame_key='d', previous_frame_key='a', next_slice_key='w', previous_slice_key='s'):
+  def __init__(self, images=[], caxis=None, cmap=plt.get_cmap('Greys_r'), title='Slice', swap_axes=None, shape=None, next_frame_key='d', previous_frame_key='a', next_slice_key='w', previous_slice_key='s'):
     self.images = images
     self.caxis = caxis
     self.cmap = cmap
     self.title = title
-    self.next_frame_key = next_frame_key
-    self.previous_frame_key = previous_frame_key
-    self.next_slice_key = next_slice_key
-    self.previous_slice_key = previous_slice_key
+    self.swap_axes = swap_axes
+    self.shape = shape
     self._images = [im.swapaxes(0, 1) for im in self.images]
+    if self.swap_axes is not None:
+      self._images = [im.swapaxes(self.swap_axes[0], self.swap_axes[1]) for im in self._images]
     self.check_dimensions()
+    self.next_frame_key     = next_frame_key
+    self.previous_frame_key = previous_frame_key
+    self.next_slice_key     = next_slice_key
+    self.previous_slice_key = previous_slice_key
     # Create figures
-    if len(self.images) < 4:
-      self.fig, self.ax = plt.subplots(1, len(self.images))
-      if len(self.images) == 1:
-        self.ax = [self.ax, None]
+    if self.shape is not None:
+      self.fig, self.ax = plt.subplots(self.shape[0], self.shape[1], layout='constrained')
     else:
-      cols = np.ceil(np.sqrt(len(self.images))).astype(int)
-      rows = np.floor(len(self.images)/cols).astype(int)
-      self.fig, self.ax = plt.subplots(rows, cols)
+      if len(self.images) < 4:
+        self.fig, self.ax = plt.subplots(1, len(self.images), layout='constrained')
+        if len(self.images) == 1:
+          self.ax = [self.ax, None]
+      else:
+        cols = np.ceil(np.sqrt(len(self.images))).astype(int)
+        rows = np.floor(len(self.images)/cols).astype(int)
+        self.fig, self.ax = plt.subplots(rows, cols, layout='tight')
 
   def check_dimensions(self):
     # Make all the images have 4 dimensions (to make all functions below generic)
@@ -34,51 +41,66 @@ class MRIPlotter:
     self.remove_keymap_conflicts({self.next_slice_key, self.previous_slice_key, self.next_frame_key, self.previous_slice_key})
     
     # Plot volumes
-    fax = self.ax.flatten()
-    for (i, im) in enumerate(self._images):
-      fax[i].im = im
-      fax[i].index = 0
-      fax[i].frame = 0
-      if self.caxis==None:
-        fax[i].imshow(im[...,fax[i].index,fax[i].frame], cmap=self.cmap, vmin=im.min(), vmax=im.max())
+    flat_ax = self.ax.flatten() # flattened axes
+    for (i, (im, ax)) in enumerate(zip(self._images, flat_ax)):
+      # Add image, index, and frame to each axis (ax)
+      ax.im = im
+      ax.slice = 0
+      ax.frame = 0
+
+      if self.caxis is None:
+        # Use minimun and maximun image values to define caxis
+        image = ax.imshow(im[..., ax.slice, ax.frame], cmap=self.cmap, vmin=im.min(), vmax=im.max())
       else:
         if isinstance(self.caxis[0], list):
-          fax[i].imshow(im[...,fax[i].index,fax[i].frame], cmap=self.cmap, vmin=self.caxis[i][0], vmax=self.caxis[i][1])
+          # Use each list contained in caxis to define minimun and maximun values
+          image = ax.imshow(im[..., ax.slice, ax.frame], cmap=self.cmap, vmin=self.caxis[i][0], vmax=self.caxis[i][1])
         else:
-          fax[i].imshow(im[...,fax[i].index,fax[i].frame], cmap=self.cmap, vmin=self.caxis[0], vmax=self.caxis[1])          
-      fax[i].invert_yaxis()
-      fax[i].set_title('Slice {:d}, frame{:d}'.format(fax[i].index,fax[i].frame))
+          # Use caxis for all images contained in _images
+          image = ax.imshow(im[..., ax.slice, ax.frame], cmap=self.cmap, vmin=self.caxis[0], vmax=self.caxis[1])
 
-    self.fig.canvas.mpl_connect('key_press_event', self.process_key)
+      # Invert axis to have y=0 at the bottom
+      ax.invert_yaxis()
+
+      # Set title to each axis
+      ax.set_title('Slice {:d}, frame{:d}'.format(ax.slice, ax.frame))
+
+      # Add colorbar
+      cbar = self.fig.colorbar(image, location='bottom', orientation='horizontal', pad=0.06)
+      cbar.minorticks_on()
+
+      # self.fig.tight_layout()
+      self.fig.canvas.mpl_connect('key_press_event', self.process_key)
+    
     plt.show()
 
   def previous_slice(self, ax):
     """Go to the previous slice."""
     im = ax.im
-    ax.index = (ax.index - 1) % im.shape[2]  # wrap around using %
-    ax.images[0].set_array(im[...,ax.index,ax.frame])
-    ax.set_title('Slice {:d}, frame {:d}'.format(ax.index, ax.frame))
+    ax.slice = (ax.slice - 1) % im.shape[2]  # wrap around using %
+    ax.images[0].set_array(im[...,ax.slice,ax.frame])
+    ax.set_title('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
 
   def next_slice(self, ax):
     """Go to the next slice."""
     im = ax.im
-    ax.index = (ax.index + 1) % im.shape[2]
-    ax.images[0].set_array(im[...,ax.index,ax.frame])
-    ax.set_title('Slice {:d}, frame {:d}'.format(ax.index, ax.frame))
+    ax.slice = (ax.slice + 1) % im.shape[2]
+    ax.images[0].set_array(im[...,ax.slice,ax.frame])
+    ax.set_title('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
 
   def previous_frame(self, ax):
     """Go to the previous frame."""
     im = ax.im
     ax.frame = (ax.frame - 1) % im.shape[3]  # wrap around using %
-    ax.images[0].set_array(im[...,ax.index,ax.frame])
-    ax.set_title('Slice {:d}, frame {:d}'.format(ax.index, ax.frame))
+    ax.images[0].set_array(im[...,ax.slice,ax.frame])
+    ax.set_title('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
 
   def next_frame(self, ax):
     """Go to the next frame."""
     im = ax.im
     ax.frame = (ax.frame + 1) % im.shape[3]
-    ax.images[0].set_array(im[...,ax.index,ax.frame])
-    ax.set_title('Slice {:d}, frame {:d}'.format(ax.index, ax.frame))
+    ax.images[0].set_array(im[...,ax.slice,ax.frame])
+    ax.set_title('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
 
   def remove_keymap_conflicts(self, new_keys_set):
     for prop in plt.rcParams:
@@ -90,7 +112,7 @@ class MRIPlotter:
 
   def process_key(self,event):
     fig = event.canvas.figure
-    for ax in fig.axes:
+    for ax in fig.axes[:len(self._images)]:
       if event.key == 's':
           self.previous_slice(ax)
       elif event.key == 'w':
